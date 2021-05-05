263c8ce59ee4b1f35c4c7a4df4d192b27f4b2629
Mark Valyavka <v221101mark@gmail.com>
2021-05-05 14:59:07 +0300

    intial ping_latency

---
+++ cmake-build-debug/CMakeFiles/test_ping.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_ping.dir/main.cpp.o"
+  "test_ping"
+  "test_ping.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_ping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
