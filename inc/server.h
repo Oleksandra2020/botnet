1fd36ffe85b581e5039751299690e5397a04edb4
ReyBroncas <andriksmen@gmail.com>
2021-06-10 12:22:02 +0300

    fixed ping clients timing

--- inc/server.h
+++ inc/server.h
@@ -1,7 +1,7 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#define INACTIVITY_TIMEOUT 5  // 5 seconds
+#define INACTIVITY_TIMEOUT 20  // 5 seconds
 #define INACTIVE_COUNTER_MAX 8
 #define NONE_PARAMETERS ""
 #define MAX_NUMBER_OF_BOTS_DATA_PER_MSG 20
