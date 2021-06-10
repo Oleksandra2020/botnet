885b165b418c9e788cef8eceb76644112d53a5e8
Rey Broncas <56468235+ReyBroncas@users.noreply.github.com>
2021-06-10 18:16:48 +0300

    Update connect_clients.sh

--- scripts/connect_clients.sh
+++ scripts/connect_clients.sh
@@ -3,5 +3,5 @@
 # shellcheck disable=SC2039
 for i in {1..1000}; do
         sleep 0.1;
-        ./build/botnet client $(( ${i} + 8200 )) <server_IP> 3916 &
+        ./build/botnet client $(( ${i} + 8200 )) 127.0.0.1 2772 &
 done
